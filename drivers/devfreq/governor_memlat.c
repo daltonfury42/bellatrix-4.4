ccaea84fc5834fd94005f3c1201ca8d3c98b6092
Saravana Kannan <skannan@codeaurora.org>
2017-09-07 22:22:49 -0700

    PM / devfreq: memlat: Don't ignore extremely latency sensitive workloads
    The ratio value that's compared against ratio_ceil tunable can be 0 for
    workloads that are extremely latency sensitive. So, we can't ignore cores
    that have a ratio of 0 (which was done to ignore idle cores). So, stop
    ignoring cores with a ratio of 0 and instead check for instruction or
    frequency being 0 to identify idle cores.
    
    Change-Id: I8c6c14f374f016e6612c3b834589d065bad8f488
    Signed-off-by: Saravana Kannan <skannan@codeaurora.org>

--- drivers/devfreq/governor_memlat.c
+++ drivers/devfreq/governor_memlat.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015-2016, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2015-2017, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -252,6 +252,7 @@ static int devfreq_memlat_get_freq(struct devfreq *df,
 					hw->core_stats[i].freq,
 					hw->core_stats[i].stall_pct, ratio);
 
+<<<<<<< HEAD
 		if (ratio <= node->ratio_ceil
 		    && hw->core_stats[i].stall_pct >= node->stall_floor
 		    && hw->core_stats[i].freq > max_freq) {
